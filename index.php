1bd65a745dc01044b375895d6b26ad2ba80e04d3
Milyra35 <marie.richir35@gmail.com>
2023-07-18 17:19:48 +0200

    Beginning ProductController while doing the structure of the project

--- index.php
+++ index.php
@@ -2,7 +2,23 @@
 
 session_start();
 
+$host = "db.3wa.io";
+$port = "3306";
+$dbName = "marierichir_ecommerce";
+$username = "marierichir";
+$password = "a616eefc0b8af8e5fb785ae6b42c19f1";
+
 require 'services/autoload.php';
+
+$um = new UserManager($dbName, $port, $host, $username, $password);
+$pm = new ProductManager($dbName, $port, $host, $username, $password);
+$cm = new CategoryManager($dbName, $port, $host, $username, $password);
+$om = new OrderManager($dbName, $port, $host, $username, $password);
+
+$uc = new UserController($um);
+$pc = new ProductController($pm);
+$cc = new CategoryController($cm);
+
 
 
 ?>
