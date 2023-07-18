feb8993d5de96dda5082d9c7967eb921dae67f40
Bigo357 <129935631+Bigo357@users.noreply.github.com>
2023-07-18 12:17:34 +0200

    OrderManager

---
+++ OrderManager.php
@@ -0,0 +1,39 @@
+<?php
+
+require 'AbstractManager.php';
+
+class OrderManager extends AbstractManager {
+
+    public function saveOrder(Order $order): bool {
+        // Supposons que vous ayez une table "orders" dans votre base de données avec les colonnes correspondantes
+
+        // Récupérer les valeurs de l'objet Order pour l'insertion dans la base de données
+        $orderId = $order->getId();
+        $date = $order->getDate()->format('Y-m-d H:i:s');
+        $productId = $order->getProduct()->getId();
+        $userId = $order->getUser()->getId();
+        $price = $order->getPrice();
+
+        // Exemple de requête d'insertion SQL
+        $sql = "INSERT INTO orders (id, date, product_id, user_id, price) VALUES (?, ?, ?, ?, ?)";
+
+        // Préparer la requête avec la connexion à la base de données
+        $stmt = $this->db()->prepare($sql);
+
+        // Binder les valeurs aux paramètres de la requête
+        $stmt->bind_param("issid", $orderId, $date, $productId, $userId, $price);
+
+        // Exécuter la requête
+        $result = $stmt->execute();
+
+        // Vérifier si l'insertion s'est bien déroulée
+        if ($result) {
+            return true; // Retourner true si la sauvegarde a réussi
+        } else {
+            return false; // Retourner false si la sauvegarde a échoué
+        }
+    }
+}
+
+
+?>
