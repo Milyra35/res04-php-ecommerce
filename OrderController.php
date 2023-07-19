979747fa2f0a21c3c501927aa1f60fe1eb405066
Bigo357 <129935631+Bigo357@users.noreply.github.com>
2023-07-19 11:53:34 +0200

    OrderManager

---
+++ OrderController.php
@@ -0,0 +1,43 @@
+<?php
+
+Class OrderController extends AbstractController {
+
+    private OrderManager $O_manager;
+
+    public function __construct(OrderController $O_manager){
+
+        $this->OrderManager = $O_manager;
+
+    }
+
+    public function createOrder(array $post){
+    // Rendu de la vue "orders/create.phtml" avec le tableau $post
+    $this->render('orders/create.phtml', $post);
+
+    if(isset($_POST['add'])){
+
+    }
+}
+
+    public function getOrderById(){
+
+    }
+
+    public function getAmoutProduct(){
+
+    }
+
+    public function getTotalPrice (){
+
+    }
+
+    public function getAllOrders(){
+
+    }
+
+    public function getOrderByUser(){
+
+    }
+}
+
+?>
